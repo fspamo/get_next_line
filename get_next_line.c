42fcd92eecf9de74c0a0cfe60277703a2a18322e
Cahit Atilla Bozkurt <cbozkurt@k2m03s01.42kocaeli.com.tr>
2026-02-18 19:46:47 +0300

    first commit, berbat kod

--- get_next_line.c
+++ get_next_line.c
@@ -13,17 +13,95 @@
 #include "get_next_line.h"
 #include <unistd.h>
 #include <stdlib.h>
+// #include <stdlib.h>
 #include <fcntl.h>
+
+char *next_lining(char *stash)
+{
+    int		i;
+    int 	j;
+    char	*line;
+
+    i = 0;
+    j = 0;
+    while (stash[i] && stash[i] != '\n')
+        i++;
+    if (stash[i] == '\n')
+        line = malloc(i + 2);
+    else
+        line = malloc(i + 1);
+    if (!line)
+        return NULL;
+    while (j < i)
+    {
+        line[j] = stash[j];
+        j++;
+    }
+    if (stash[i] == '\n')
+    {
+        line[j] = '\n';
+        j++;
+    }
+
+    line[j] = '\0';
+    return line;
+}
+
+char	*read_as_buffer(int fd, char *stash)
+{
+	char	*buffer;
+	size_t 	bytes_to_read;
+	int i;
+
+	i = 0;
+	buffer = malloc(BUFFER_SIZE + 1);
+	bytes_to_read = read(fd, buffer, BUFFER_SIZE);
+	//strjoin
+	if (!stash)
+		stash = malloc(bytes_to_read + 1);
+	while (i < bytes_to_read)
+	{
+		stash[i] = buffer[i];
+		i++;
+	}
+	return (stash);
+}
+
+char	*clean_stash(char *stash)
+{
+	int 	i;
+	int 	j;
+	char	*new;
+	
+	i = 0;
+	j = 0;
+	while (stash[i] != '\n')
+		i++;
+	i++;
+	new = malloc((ft_strlen(stash) - i) + 1);
+	if (!new)
+		return (NULL);
+	while (stash[i])
+	{
+		new[j] = stash[i];
+		i++;
+		j++;
+	}
+	new[j] = '\0';
+	return (new);
+}
 
 char	*get_next_line(int fd)
 {
-	char	*buf;
-	int		nbytes;
+	static char	*stash;
+	char 		*chosen_one;
 
-	nbytes = read(fd, buf, BUFFER_SIZE);
-	buf = malloc(BUFFER_SIZE + 1);
-	buf[nbytes] = '\0';
-	return (buf);
+	stash = read_as_buffer(fd, stash);
+	chosen_one = next_lining(stash);
+
+	stash = clean_stash(stash);
+	
+	return (chosen_one);
 }
 
 #include <stdio.h>
--- get_next_line.h
+++ get_next_line.h
@@ -14,9 +14,14 @@
 # define GET_NEXT_LINE_H
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
+# define BUFFER_SIZE 1
 #endif
 
+#include <stddef.h>
+
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int ch);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
 
 #endif

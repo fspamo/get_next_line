6cd1b75658600221ba700c4bd6d51366b7cbe357
Cahit Atilla Bozkurt <cbozkurt@k2m05s04.42kocaeli.com.tr>
2026-02-19 10:47:34 +0300

    free'd, safed and got the next line lol

--- get_next_line.c
+++ get_next_line.c
@@ -13,103 +13,126 @@
 #include "get_next_line.h"
 #include <unistd.h>
 #include <stdlib.h>
-// #include <stdlib.h>
+#include <stdio.h>
 #include <fcntl.h>
 
 char *next_lining(char *stash)
 {
-    int		i;
-    int 	j;
-    char	*line;
+	int		i;
+	int 	j;
+	char	*line;
+
+	i = 0;
+	j = 0;
+	while (stash[i] && stash[i] != '\n')
+		i++;
+	if (stash[i] == '\n')
+		line = malloc(i + 2);
+	else
+		line = malloc(i + 1);
+	if (!line)
+		return NULL;
+	while (j < i)
+	{
+		line[j] = stash[j];
+		j++;
+	}
+	if (stash[i] == '\n')
+	{
+		line[j] = '\n';
+		j++;
+	}
+
+	line[j] = '\0';
+	return line;
+}
+
+char	*read_as_buffer(int fd, char *stash)
+{
+	char	*buffer;
+	char	*holder;
+	int		bytes_to_read;
+
+	if (!stash)
+	{
+		stash = malloc(1);
+		stash[0] = '\0';
+	}
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	bytes_to_read = 1;
+	while ((!stash || !ft_strchr(stash, '\n')) && bytes_to_read != 0)
+	{
+		bytes_to_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_to_read == -1)
+		{
+			free(buffer);
+			free(stash);
+			return (NULL);
+		}
+		buffer[bytes_to_read] = '\0';
+		holder = ft_strjoin(stash, buffer);
+		free(stash);
+		stash = holder;
+	}
+	free(buffer);
+	return (stash);
+}
+
+char *clean_stash(char *stash)
+{
+    int     i;
+    int     j;
+    char    *new;
 
     i = 0;
     j = 0;
     while (stash[i] && stash[i] != '\n')
         i++;
     if (stash[i] == '\n')
-        line = malloc(i + 2);
-    else
-        line = malloc(i + 1);
-    if (!line)
-        return NULL;
-    while (j < i)
+        i++;
+    if (!stash[i])
     {
-        line[j] = stash[j];
-        j++;
+        new = malloc(1);
+        if (!new)
+            return (NULL);
+        new[0] = '\0';
+        free(stash);
+        return (new);
     }
-    if (stash[i] == '\n')
-    {
-        line[j] = '\n';
-        j++;
-    }
-
-    line[j] = '\0';
-    return line;
+    new = malloc(ft_strlen(stash) - i + 1);
+    if (!new)
+        return (NULL);
+    while (stash[i])
+        new[j++] = stash[i++];
+    new[j] = '\0';
+    free(stash);
+    return (new);
 }
 
-char	*read_as_buffer(int fd, char *stash)
+char *get_next_line(int fd)
 {
-	char	*buffer;
-	size_t 	bytes_to_read;
-	int i;
-
-	i = 0;
-	buffer = malloc(BUFFER_SIZE + 1);
-	bytes_to_read = read(fd, buffer, BUFFER_SIZE);
-	//strjoin
-	if (!stash)
-		stash = malloc(bytes_to_read + 1);
-	while (i < bytes_to_read)
-	{
-		stash[i] = buffer[i];
-		i++;
-	}
-	return (stash);
-}
-
-char	*clean_stash(char *stash)
-{
-	int 	i;
-	int 	j;
-	char	*new;
-	
-	i = 0;
-	j = 0;
-	while (stash[i] != '\n')
-		i++;
-	i++;
-	new = malloc((ft_strlen(stash) - i) + 1);
-	if (!new)
-		return (NULL);
-	while (stash[i])
-	{
-		new[j] = stash[i];
-		i++;
-		j++;
-	}
-	new[j] = '\0';
-	return (new);
-}
-
-char	*get_next_line(int fd)
-{
-	static char	*stash;
-	char 		*chosen_one;
+	static char *stash;
+	char        *chosen_one;
 
 	stash = read_as_buffer(fd, stash);
+	if (!stash || stash[0] == '\0')  // EOF kontrolü
+		return (NULL);
 	chosen_one = next_lining(stash);
-
 	stash = clean_stash(stash);
-	
 	return (chosen_one);
 }
-
-#include <stdio.h>
-
-int main(void)
-{
-	int return_val;
-
-	return_val = open("test.txt", O_CREAT | O_RDONLY, 0777);
-	printf("%s", get_next_line(return_val));
-}
+//
+// #include <stdio.h>
+//
+// int main(void)
+// {
+// 	int return_val;
+//
+// 	return_val = open("test.txt", O_RDONLY);
+// 	printf("%s", get_next_line(return_val));
+// 	printf("%s", get_next_line(return_val));
+// 	printf("%s", get_next_line(return_val));
+// 	printf("%s", get_next_line(return_val));
+// }
